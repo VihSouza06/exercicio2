d3d8ddaa2e5d97653b44ea1f312d6f4ab3bee873
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-14 20:55:41 -0300

    Criação do service

---
+++ exercicio2/src/main/java/com/bn/exercicio2/services/ProdutoService.java
@@ -0,0 +1,35 @@
+package com.bn.exercicio2.services;
+
+import com.bn.exercicio2.models.ProdutoModel;
+import com.bn.exercicio2.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoModel> findAll(){
+        return produtoRepository.findAll();
+    }
+
+    public ProdutoModel criarProduto(ProdutoModel produtoModel){
+        return produtoRepository.save(produtoModel);
+    }
+
+    public void deletarProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+    public ProdutoModel buscarProdutoPorId(Long id){
+        return produtoRepository.findById(id).get();
+    }
+
+    public ProdutoModel atualizarProduto(Long id, ProdutoModel produtoModel){
+        ProdutoModel newProdutoModel = produtoRepository.findById(id).get();
+        return produtoRepository.save(produtoModel);
+    }
+}
