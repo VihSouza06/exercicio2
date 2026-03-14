2efca5b2466fb6479c566db81d3d7a4ded78d7b7
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-14 19:18:12 -0300

    gerenciamento de produtos

---
+++ exercicio2/src/main/java/com/bn/exercicio2/models/ProdutoModel.java
@@ -0,0 +1,55 @@
+package com.bn.exercicio2.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class ProdutoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private Integer estoque;
+
+    public ProdutoModel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
